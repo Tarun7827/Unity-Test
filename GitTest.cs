0295f1d004cdd089b3f4fd62857b362cdb7ceff9
Tarun7827 <43825058+Tarun7827@users.noreply.github.com>
2018-10-04 00:00:07 +0530

    Add files via upload

---
+++ GitTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3 (1,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
