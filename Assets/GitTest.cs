ca43d96dd3c0755ef6ec581c531b9f9f194798b7
Tarun7827 <43825058+Tarun7827@users.noreply.github.com>
2018-10-04 00:01:05 +0530

    Add files via upload

--- Assets/GitTest.cs
+++ Assets/GitTest.cs
@@ -6,7 +6,7 @@ public class GitTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		transform.position = new Vector3 (1,0,0);
 	}
 	
 	// Update is called once per frame
